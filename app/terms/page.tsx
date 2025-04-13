d3380e447a4a48174acb3edfd36bc00c3ea4a60d
oyedelehabeeb <hoyedeley4@gmail.com>
2025-04-13 09:36:58 +0100

    feat: implement session validation and redirect to login for protected pages

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,6 +1,17 @@
 import Terms from "@/components/TermsPage";
+import { supabase } from "@/services/supabase";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  // Get the session on the server
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // If no session, redirect to the home page
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div>
       <Terms />
