d56ad09a93d943b419b1df13b50f8cccc479acc9
oyedelehabeeb <hoyedeley4@gmail.com>
2025-05-02 20:31:07 +0100

    fix: add login redirect for unauthorized users in DisclaimerPage component

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -3,14 +3,5 @@ import Disclaimer from "@/components/DisclaimerPage";
 // import { redirect } from "next/navigation";
 
 export default async function Page() {
-  // Get the session on the server
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
-
-  // // If no session, redirect to the home page
-  // if (!session) {
-  //   redirect("/login");
-  // }
   return <Disclaimer />;
 }
